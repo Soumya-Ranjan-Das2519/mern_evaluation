7f276532597b4807f815c8fced3b469fd921032c
soumya <sd7557920@gmail.com>
2024-12-23 13:16:22 +0530

    react display name

---
+++ react_code/displayName/src/components/Hello.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+// eslint-disable-next-line react/prop-types
+const Hello = ({ name }) => {
+  return <h1>Hello, {name}</h1>;
+};
+
+export default Hello;
